cb1b409b92f9c23b8031f11ebec4da010b3a40c6
sharp <674106780@qq.com>
2025-01-29 22:33:24 +0800

    update day4

--- day4/bootpack.cpp
+++ day4/bootpack.cpp
@@ -1,18 +1,36 @@
 #include "bootpack.h"
 
+
+
 int os_main()
 {
 
     CPalette pal;
     pal.init_palette();
+    // pal.boxfill(Color8::COL8_FF0000,20, 20, 120, 120);
+    // pal.boxfill(Color8::COL8_00FF00,70, 50, 170, 150);
+    // pal.boxfill(Color8::COL8_0000FF,120, 80, 220, 180);
+    uint xsize = pal.get_xsize();
+    uint ysize = pal.get_ysize();
 
-    char* vga_mem_base = (char*)0xa0000;
+    pal.boxfill(Color8::COL8_008484, 0, 0, xsize - 1, ysize - 29);
+    pal.boxfill(Color8::COL8_E6E6E6, 0, ysize - 28, xsize - 1, ysize - 28);
+    pal.boxfill(Color8::COL8_FFFFFF, 0, ysize - 27, xsize - 1, ysize - 27);
+    pal.boxfill(Color8::COL8_E6E6E6, 0, ysize - 26, xsize - 1, ysize - 1);
 
-    for(uint i=0; i<= 0xffff; ++i)
-    {
-        *(vga_mem_base + i) = i & 0x0f;
-    }
+    pal.boxfill(Color8::COL8_FFFFFF, 3, ysize - 24, 59, ysize - 24);
+    pal.boxfill(Color8::COL8_FFFFFF, 2, ysize - 24, 2, ysize - 4);
+    pal.boxfill(Color8::COL8_848484, 3, ysize - 4, 59, ysize - 4);
+    pal.boxfill(Color8::COL8_848484, 59, ysize - 23, 59, ysize - 5);
+    pal.boxfill(Color8::COL8_000000, 2, ysize - 3, 59, ysize - 3);
+    pal.boxfill(Color8::COL8_000000, 60, ysize - 24, 60, ysize - 3);
     
+    pal.boxfill(Color8::COL8_848484, xsize - 47, ysize - 24, xsize - 4, ysize - 24);
+    pal.boxfill(Color8::COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize - 4);
+    pal.boxfill(Color8::COL8_FFFFFF, xsize - 47, ysize - 3, xsize - 4, ysize - 3);
+    pal.boxfill(Color8::COL8_FFFFFF, xsize - 3, ysize - 24, xsize - 3, ysize - 3);
+
+
     for(;;)
     {
         asm{
--- day4/bootpack.h
+++ day4/bootpack.h
@@ -5,13 +5,40 @@
 
 using uint = unsigned int;
 
+enum Color8 {
+    COL8_000000=0,
+    COL8_FF0000=1,
+    COL8_00FF00=2,
+    COL8_FFFF00=3,
+    COL8_0000FF=4,
+    COL8_FF00FF=5,
+    COL8_00FFFF=6,
+    COL8_FFFFFF=7,
+    COL8_E6E6E6=8,
+    COL8_840000=9,
+    COL8_008400=10,
+    COL8_848400=11,
+    COL8_000084=12,
+    COL8_840084=13,
+    COL8_008484=14,
+    COL8_848484=15
+
+};
+
 
 class CPalette
 {
 public:
+    CPalette() : _vram((char*)0xa0000), _xsize(320), _ysize(200) {}
     void init_palette();
-    CPalette() = default;
+    void boxfill(Color8 color, uint x0, uint y0, uint x1, uint y1);
+    uint get_xsize() {return _xsize;}
+    uint get_ysize() {return _ysize;}
+
 private:
+    char* _vram;
+    uint _xsize, _ysize;
+
     static unsigned char table_rgb[] ;
 
 };
@@ -20,7 +47,7 @@ void CPalette::init_palette()
 {
     constexpr uint start = 0;
     constexpr uint size = 16; 
-    int i, eflags;
+    uint i, eflags;
     eflags = io_load_eflags();
     io_cli();
     io_out8(0x03c8, start);
@@ -34,6 +61,13 @@ void CPalette::init_palette()
 
 }
 
+void CPalette::boxfill(Color8 color, uint x0, uint y0, uint x1, uint y1)
+{
+    for(uint y = y0; y <= y1; ++y)
+        for(uint x = x0; x <= x1; ++x)
+            _vram[y*_xsize + x] = color;
+
+}
 
 unsigned char CPalette::table_rgb[] = {
         0x00,0x00,0x00,   //0:黑 
@@ -44,7 +78,7 @@ unsigned char CPalette::table_rgb[] = {
         0xff, 0x00, 0xff, //5:亮紫 
         0x00, 0xff, 0xff, //6:浅亮蓝 
         0xff, 0xff, 0xff, //7:白 
-        0xc6, 0xc6, 0xc6, //8:亮灰 
+        0xe6, 0xe6, 0xe6, //8:亮灰 
         0x84, 0x00, 0x00, //9:暗红 
         0x00, 0x84, 0x00, //10:暗绿 
         0x84, 0x84, 0x00, //11:暗黄 
