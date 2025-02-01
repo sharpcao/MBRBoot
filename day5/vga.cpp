7d4af0ac33636b49e79552ec6bc3b6289788747d
sharp <674106780@qq.com>
2025-02-01 11:04:27 +0800

    tidy code

--- day5/vga.cpp
+++ day5/vga.cpp
@@ -3,24 +3,17 @@
 void CPalette::init_cursor()
 {
     for(uint i = 0; i < 16; ++i)
-    {
         for(uint j=0; j<16; ++j)
-        {
             _cursor_image[i*16+j] = (_cursor_raw[i][j]=='1')? (Color8::COL8_FFFFFF):(Color8::COL8_008484);
-        }
-    }
 
 }
 
 void CPalette::draw_rect(uint x, uint y,uint width, uint height, char* buf)
 {
     for (uint i = 0; i<height; ++i)
-    {
         for(uint j = 0; j < width; ++j)
-        {
             _vram[(y+i)*_xsize + x + j] = buf[i*width + j];
-        }
-    }
+
 }
 
 void CPalette::draw_cursor(uint x, uint y)
@@ -48,7 +41,6 @@ void CPalette::init_palette()
 
 void CPalette::boxfill(Color8 color, uint x0, uint y0, uint x1, uint y1)
 {
-    uint _xsize = get_xsize();
     for(uint y = y0; y <= y1; ++y)
         for(uint x = x0; x <= x1; ++x)
             _vram[y*_xsize + x] = color;
@@ -60,25 +52,21 @@ void CPalette::putfont8(uint x, uint y, uchar asc, Color8 col)
     uchar* asc_data = _pfont+ asc * 16;
     for(uint i =0; i < 16; ++i)
     {
-
-        char* pos = _vram + (y + i) * get_xsize() + x;
+        char* pos = _vram + (y + i) * _xsize + x;
         for(uint j=0; j < 8 ; ++j)
         {
             char b = (0b10000000 >> j);
-            if(asc_data[i] & b){
-                pos[j] =col;
-
-            }
+            if(asc_data[i] & b) { pos[j] = col;}
         }
 
     }
 }
+
 void CPalette::xyprint(uint x, uint y, const char* str, Color8 col)
 {
     for(;*str != 0; ++str, x+=8)
-    {
         putfont8(x,y,*str,col);
-    }
+
 }
 
 void CPalette::draw_desktop()
@@ -99,6 +87,5 @@ void CPalette::draw_desktop()
     boxfill(Color8::COL8_848484, _xsize - 47, _ysize - 23, _xsize - 47, _ysize - 4);
     boxfill(Color8::COL8_FFFFFF, _xsize - 47, _ysize - 3, _xsize - 4, _ysize - 3);
     boxfill(Color8::COL8_FFFFFF, _xsize - 3, _ysize - 24, _xsize - 3, _ysize - 3);
-
 
 }
